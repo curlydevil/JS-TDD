7a8cfa855e268bd5cbd0e17abeb23de785511695
Denis B <dberzoy@gmail.com>
2015-07-02 13:45:26 +0300

    test 2

--- kata-02/test/test.js
+++ kata-02/test/test.js
@@ -4,5 +4,9 @@ var FizzBuzz = require("../lib/fizzbuzz.js")
 describe('FizzBuzz', function(){
     it('should exist', function(){
         FizzBuzz.should.be;
-    })
+    });
+
+    it("should return Fizz when input mod 3 equals 0", function(){
+        FizzBuzz(3).should.be('Fizz');
+    });
 });

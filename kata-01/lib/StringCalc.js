0cf04b9223ba83b740f21d51eb05b8f17a7f2d12
Denis B <dberzoy@gmail.com>
2015-07-02 12:46:03 +0300

    test 8

--- kata-01/lib/StringCalc.js
+++ kata-01/lib/StringCalc.js
@@ -1,6 +1,14 @@
 function StringCalc(input){
     if(!input || input === '')
         return 0;
+
+    if(input.substr(0,1) == "//")
+    {
+        var customDelimiter = input.substr(2,1);
+        console.log('delim', customDelimiter);
+        input = input.replace(customDelimiter, ',');
+    }
+
     input = input.replace('\n',',')
     var array = input.split(',');
     var sum = 0;

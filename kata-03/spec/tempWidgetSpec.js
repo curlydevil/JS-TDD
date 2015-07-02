a0f42bc28f9fe1dfd0e6248ceb0fe44b8d4cf0ea
Denis B <dberzoy@gmail.com>
2015-07-02 14:34:05 +0300

    kata-3

---
+++ kata-03/spec/tempWidgetSpec.js
@@ -0,0 +1,16 @@
+// display temperature (ajax call)
+// display update button
+// when user clicks on button widget should update temperature
+
+describe("TempWidget", function(){
+    var $parent, widget;
+
+    beforeEach(function(){
+        $parent = $('<div class="test-parent"></div>').appendTo('body');
+        widget = new TempWidget($parent);
+    });
+
+
+
+
+});

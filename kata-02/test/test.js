1f9480ae76c8c9f060dee2f19da25bd01476347d
Denis B <dberzoy@gmail.com>
2015-07-02 14:00:10 +0300

    test 3

--- kata-02/test/test.js
+++ kata-02/test/test.js
@@ -6,14 +6,6 @@ describe('FizzBuzz', function(){
         FizzBuzz.should.be;
     });
 
-    it("should return input when input mod 3 != 0", function(){
-        FizzBuzz(2).should.equal(2);
-    });
-
-    it("should return input when input mod 5 != 0", function(){
-        FizzBuzz(7).should.equal(7);
-    });
-
     it("should return Fizz when input mod 3 == 0", function(){
         FizzBuzz(3).should.equal('Fizz');
         FizzBuzz(6).should.equal('Fizz');
@@ -29,4 +21,8 @@ describe('FizzBuzz', function(){
     });*/
 
 
+    it("should return input in all other cases", function(){
+        FizzBuzz(2).should.equal(2);
+        FizzBuzz(7).should.equal(7);
+    });
 });

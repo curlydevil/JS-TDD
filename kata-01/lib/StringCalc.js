79f9b59678425743feac142187c49e68ba2b1a5e
Denis B <dberzoy@gmail.com>
2015-07-02 12:24:47 +0300

    4th test

--- kata-01/lib/StringCalc.js
+++ kata-01/lib/StringCalc.js
@@ -1,5 +1,7 @@
-function StringCalc(){
-return 0;
+function StringCalc(value){
+    if(value === '')
+        return 0;
+    else return 5;
 }
 
 module.exports = StringCalc;
--- kata-01/spec/string_calc_spec.js
+++ kata-01/spec/string_calc_spec.js
@@ -10,7 +10,17 @@ describe("String calculator", function(){
         expect(StringCalc()).toBeDefined();
     });
 
-    it("should return 0 when input is ''", function(){
-        expect(StringCalc('')).toEqual(0);
+    describe("when input is empty string", function(){
+        it("should return 0", function(){
+            expect(StringCalc('')).toEqual(0);
+        });
     });
+
+    describe("when input is a value as string", function(){
+       it("should return same value as number", function(){
+           expect(StringCalc('5')).toEqual(5);
+       });
+
+    });
+
 });

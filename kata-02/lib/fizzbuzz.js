530cc12090c47953bc9ebba4f0f06c9b732667af
Denis B <dberzoy@gmail.com>
2015-07-02 14:06:41 +0300

    test 3

--- kata-02/lib/fizzbuzz.js
+++ kata-02/lib/fizzbuzz.js
@@ -1,8 +1,8 @@
 function FizzBuzz(input){
-    var temp = input;
-    if (input % 3 === 0) temp = 'Fizz';
-    if (input % 5 === 0) temp += ' Buzz';
-    else return temp;
+    var temp = [];
+    if (input % 3 === 0) temp.push('Fizz');
+    if (input % 5 === 0) temp = ('Buzz');
+    return (temp.length == 0) ? input : temp.join(' ');
 }
 
 module.exports = FizzBuzz;

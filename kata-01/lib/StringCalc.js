e029c35a406460f2fa5417018afe25d0587a32dd
Denis B <dberzoy@gmail.com>
2015-07-02 12:40:07 +0300

    test 7

--- kata-01/lib/StringCalc.js
+++ kata-01/lib/StringCalc.js
@@ -1,6 +1,7 @@
 function StringCalc(input){
     if(!input || input === '')
         return 0;
+    input = input.replace('\n',',')
     var array = input.split(',');
     var sum = 0;
     for(var i = 0; i< array.length; i++){
